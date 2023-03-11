bdd538472cdaf88f99f13e83e37bfa47d1e699ee
daniDF <danieledraghi2@gmail.com>
2023-03-11 14:13:07 +0100

    Added command line board dim

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from move import Move
 
 import time
 import os
+import sys
 
 
 def user_play(game):
@@ -35,8 +36,7 @@ def player_play(game):
     player.learn(episodes=game.board_dim*2000, board_dim=game.board_dim)
     print("Stop learning")
 
-    print(game)
-    print()
+    on_move_played(game,None)
     player.play_game(game=game, on_move_played=on_move_played)
 
 
@@ -51,12 +51,17 @@ def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def main():
-    game = Game(board_dim=6)
+def main(argv):
+    board_dim = 6
+
+    if len(argv) > 1:
+        board_dim = int(argv[1])
+
+    game = Game(board_dim=board_dim)
 
     player_play(game)
     # user_play(game)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
